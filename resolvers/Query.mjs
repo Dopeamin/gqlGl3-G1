c9f465de855ddad0cdf50db263dd2cd0bc807153
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2022-04-26 23:45:36 +0100

    add comments and code format

--- resolvers/Query.mjs
+++ resolvers/Query.mjs
@@ -1,9 +1,13 @@
 export const Query = {
   hello: (_, { name }) => `Hello ${name || "World"}`,
+  
+  // Get all of the todos
   getTodos: (parent, args, { db }, info) => {
     console.log(db);
     return db.todos;
   },
+
+  //Fetch a specific todo by Id
   getTodoById: (parent, { id }, { db }, info) => {
     const todo = db.todos.find((todo) => todo.id === id);
     if (!todo) {
@@ -11,9 +15,13 @@ export const Query = {
     }
     return todo;
   },
+
+  //Fetch all users
   getUsers: (parent, args, { db }, info) => {
     return db.users;
   },
+
+  //Fetch a user by Id
   getUserById: (parent, { id }, { db }, info) => {
     const user = db.users.find((user) => user.id === id);
     if (!user) {
